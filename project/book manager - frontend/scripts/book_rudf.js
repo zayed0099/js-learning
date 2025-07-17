dfeb6750ba5249ab6a1dcbeb87fefabdc482e1e6
Zayed Ahmed <zayed.ah06@gmail.com>
2025-07-17 19:32:57 +0600

    added favourites add/remove option

--- project/book manager - frontend/scripts/book_rudf.js
+++ project/book manager - frontend/scripts/book_rudf.js
@@ -19,48 +19,117 @@ document.addEventListener('DOMContentLoaded', () => {
 			        return;
 			    }
 
-            books.forEach(item => {
+            books.forEach(item => {                  
                   const row = document.createElement('tr');
 
                   const titleCell = document.createElement('td');
                   titleCell.textContent = item.title;
                   row.appendChild(titleCell);
 
-                // Show more (Edit + Delete + Favourites)
+    //(Edit + Delete + Favourites)
+    
           				// Code for DELETE option.
           				const buttonCell = document.createElement('td');
           				const delBtn = document.createElement('button');
           				delBtn.textContent = 'Delete';
+                  const url_del = `http://127.0.0.1:5000/api/v1/books/${item.id}`;
 
           				// Add event listener for button click
           				delBtn.addEventListener('click', () => {
           				    alert(
           				        `Title: ${item.title} will be deleted.`
           				    );
-
-                              url_del = `http://127.0.0.1:5000/api/v1/books/${item.id}`
-                              fetch(url_del, {
-                                    method: 'DELETE',
-                                    headers: {
-                                      'Authorization': `Bearer ${token}`}
-                                  })
-                                  .then(response => {
-                                    if (!response.ok) {
-                                      throw new Error('Network response was not ok ' + response.statusText);
-                                    }
-                                    // Handle successful deletion (e.g., remove from UI)
-                                    console.log('Resource deleted successfully');
-                                    alert('Book deleted successfully.');
-                                    location.reload();
-                                  })
-                                  .catch(error => {
-                                    // Handle errors (e.g., display error message)
-                                    console.error('There was a problem with the fetch operation:', error);
-                              });
+                      fetch(url_del, {
+                            method: 'DELETE',
+                            headers: {
+                              'Authorization': `Bearer ${token}`}
+                          })
+                          .then(response => {
+                            if (!response.ok) {
+                              throw new Error('Network response was not ok ' + response.statusText);
+                            }
+                            // Handle successful deletion (e.g., remove from UI)
+                            console.log('Resource deleted successfully');
+                            alert('Book deleted successfully.');
+                            location.reload();
+                          })
+                          .catch(error => {
+                            // Handle errors (e.g., display error message)
+                            console.error('There was a problem with the fetch operation:', error);
+                      });
           				});
 
           				buttonCell.appendChild(delBtn);
-          				row.appendChild(buttonCell);
+          				row.appendChild(buttonCell); // delete button ends
+
+                  // Code for Add Favourite option.
+                  const buttonCell2 = document.createElement('td');
+                  const addFavBtn = document.createElement('button');
+                  addFavBtn.textContent = 'Add Fav';
+                  const url_add_fav = `http://127.0.0.1:5000/api/v1/favourites/${item.id}`;
+
+                  // Add event listener for button click
+                  addFavBtn.addEventListener('click', () => {
+                      alert(
+                          `Title: ${item.title} will be added as favourite.`
+                      );
+                      fetch(url_add_fav, {
+                            method: 'PUT',
+                            headers: {
+                              'Authorization': `Bearer ${token}`}
+                          })
+                          .then(response => {
+                            if (!response.ok) {
+                              throw new Error('Network response was not ok ' + response.statusText);
+                            }
+                            // Handle successful deletion (e.g., remove from UI)
+                            console.log('Resource added as favourite successfully');
+                            alert('Book added as favourite successfully.');
+                            location.reload();
+                          })
+                          .catch(error => {
+                            // Handle errors (e.g., display error message)
+                            console.error('There was a problem with the fetch operation:', error);
+                      });
+                  });
+
+                  buttonCell2.appendChild(addFavBtn);
+                  row.appendChild(buttonCell2); // add Favourite button ends
+                
+
+                // Code for delete from Favourite option.
+                  const buttonCell3 = document.createElement('td');
+                  const delFavBtn = document.createElement('button');
+                  delFavBtn.textContent = 'Del Fav';
+                  const url_del_fav = `http://127.0.0.1:5000/api/v1/favourites/${item.id}`;
+
+                  // Add event listener for button click
+                  delFavBtn.addEventListener('click', () => {
+                      alert(
+                          `Title: ${item.title} will be removed from favourite.`
+                      );
+                      fetch(url_del_fav, {
+                            method: 'DELETE',
+                            headers: {
+                              'Authorization': `Bearer ${token}`}
+                          })
+                          .then(response => {
+                            if (!response.ok) {
+                              throw new Error('Network response was not ok ' + response.statusText);
+                            }
+                            // Handle successful deletion (e.g., remove from UI)
+                            console.log('Resource removed from favourite successfully');
+                            alert('Book removed from favourite successfully.');
+                            location.reload();
+                          })
+                          .catch(error => {
+                            // Handle errors (e.g., display error message)
+                            console.error('There was a problem with the fetch operation:', error);
+                      });
+                  });
+
+                  buttonCell3.appendChild(delFavBtn);
+                  row.appendChild(buttonCell3); // delete from Favourite button ends                
                 
                 // Appending all those data
               tableBody.appendChild(row);
