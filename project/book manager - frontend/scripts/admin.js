219393e69d179699c388e7d838108415c103f092
Zayed Ahmed <zayed.ah06@gmail.com>
2025-07-21 20:11:37 +0600

    separated user and admin based tables and made prototype of managing admins

--- project/book manager - frontend/scripts/admin.js
+++ project/book manager - frontend/scripts/admin.js
@@ -1,9 +1,11 @@
 // console.log('connected')
+let selectedadmin = null;
 
 const token = localStorage.getItem('jwtToken');; // or set it manually
-const tableBody = document.getElementById('allUserBooks'); // your tbody should have id="api-table-body"
 
-fetch('http://127.0.0.1:5000/a/v1/books', {
+const tableBody2 = document.getElementById('userTable');
+// Show all Admin Data
+fetch('http://127.0.0.1:5000/a/v1/manage', {
 method: 'GET',
 headers: {
   'Authorization': `Bearer ${token}`
@@ -12,36 +14,107 @@ headers: {
 .then(response => {
   if (!response.ok) {
     throw new Error('Network response was not ok');
-    tableBody.innerHTML = '<tr><td colspan="3"><i>Error loading data.</i></td></tr>';
+    tableBody2.innerHTML = '<tr><td colspan="3"><i>Error loading data.</i></td></tr>';
   }
   return response.json();
 })
 
 .then(data => {
-  const books = data.books
-  books.forEach(item => {
+  const admins = data.admins
+  admins.forEach(item => {
     const row = document.createElement('tr');
 
-    const titleCell = document.createElement('td');
-    titleCell.textContent = item.title;
-    row.appendChild(titleCell);
+    const usernamecell = document.createElement('td');
+    usernamecell.textContent = item.username;
+    row.appendChild(usernamecell);
 
-    const authorCell = document.createElement('td');
-    authorCell.textContent = item.author;
-    row.appendChild(authorCell);
+    const emailcell = document.createElement('td');
+    emailcell.textContent = item.email;
+    row.appendChild(emailcell);
 
-    const delCell = document.createElement('td');
-    delCell.textContent = item.is_deleted;
-    row.appendChild(delCell);
+    const updateCell = document.createElement('td');
+    const updateBtn = document.createElement('button');
+    updateBtn.textContent = 'Update';
 
-    const useridCell = document.createElement('td');
-    useridCell.textContent = item.user_id;
-    row.appendChild(useridCell);
+    updateBtn.addEventListener('click', () => {
+        selectedbook = item
+        showModal(item); // this function will open the modal and populate it
+    });
 
-    tableBody.appendChild(row);
+    updateCell.appendChild(updateBtn);
+    row.appendChild(updateCell);
+
+    tableBody2.appendChild(row);
   });
 })
 .catch(error => {
   console.error('Error fetching data:', error);
-  tableBody.innerHTML = '<tr><td colspan="3"><i>Error loading data.</i></td></tr>';
+  tableBody2.innerHTML = '<tr><td colspan="3"><i>Error loading data.</i></td></tr>';
+}); 
+
+
+// Show modal code
+// Modal control logic
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeBtn");
+const modalTitle = document.getElementById("modalTitle");
+const modalAuthor = document.getElementById("modalAuthor");
+const modalGenre = document.getElementById("modalGenre");
+const submitBtn = document.getElementById("submitChanges");
+
+const token_ = localStorage.getItem('jwtToken');
+
+function showModal(book) {
+  modalTitle.textContent = book.title;
+  modalAuthor.textContent = "Author: " + book.author;
+  modalGenre.textContent = "Genre: " + book.genre;
+  modal.style.display = "flex";
+} // for function
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
 });
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+
+submitBtn.addEventListener('click', () => {
+  const titleCng = document.getElementById('modalTitlecng').value.trim();
+  const authorCng = document.getElementById('modalAuthorcng').value.trim();
+  const genreCng = document.getElementById('modalGenrecng').value.trim();
+
+  let dataToSend = {};
+
+  if (titleCng) {dataToSend.title = titleCng;}
+  if (authorCng) {dataToSend.author = authorCng;}
+  if (genreCng) {dataToSend.genre = genreCng;}
+
+  if (!selectedbook) {
+    alert("No book selected for update.");
+    return;}
+
+  const url_update_data = `http://127.0.0.1:5000/api/v1/books/${selectedbook.id}`;
+
+  fetch(url_update_data, {
+    method: 'PATCH', // or 'PATCH' if updating
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token_}` // if using JWT
+    },
+    body: JSON.stringify(dataToSend)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    alert('Data updated successfully')
+    selectedbook = null;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+}); // for submitBtn
---
+++ project/book manager - frontend/scripts/user.js
@@ -0,0 +1,49 @@
+// console.log('connected')
+
+const token = localStorage.getItem('jwtToken');; // or set it manually
+const tableBody1 = document.getElementById('allUserBooks'); 
+
+fetch('http://127.0.0.1:5000/a/v1/books', {
+method: 'GET',
+headers: {
+  'Authorization': `Bearer ${token}`
+}
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+    tableBody1.innerHTML = '<tr><td colspan="3"><i>Error loading data.</i></td></tr>';
+  }
+  return response.json();
+})
+
+.then(data => {
+  const books = data.books
+  books.forEach(item => {
+    const row = document.createElement('tr');
+
+    const titleCell = document.createElement('td');
+    titleCell.textContent = item.title;
+    row.appendChild(titleCell);
+
+    const authorCell = document.createElement('td');
+    authorCell.textContent = item.author;
+    row.appendChild(authorCell);
+
+    const delCell = document.createElement('td');
+    delCell.textContent = item.is_deleted;
+    row.appendChild(delCell);
+
+    const useridCell = document.createElement('td');
+    useridCell.textContent = item.user_id;
+    row.appendChild(useridCell);
+
+    tableBody1.appendChild(row);
+  });
+})
+.catch(error => {
+  console.error('Error fetching data:', error);
+  tableBody1.innerHTML = '<tr><td colspan="3"><i>Error loading data.</i></td></tr>';
+});
+
+
