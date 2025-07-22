ad8ddec44aaf865523c146eebbd6c84df79afba0
Zayed Ahmed <zayed.ah06@gmail.com>
2025-07-22 20:08:37 +0600

    scripts/user.js;added users password change by admin feature

--- project/book manager - frontend/scripts/user.js
+++ project/book manager - frontend/scripts/user.js
@@ -186,3 +186,45 @@ submitBtn.addEventListener('click', () => {
   } // else if ends
 
 }); // to end the eventlistener added in submit button
+
+      
+      
+
+      // Changing users password
+document.getElementById('sendDataBtn').addEventListener('click', () => {
+
+    const username = document.getElementById('usernameInput').value;
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passInput').value;
+
+    const dataToSend = {
+      username : username,
+      password : password,
+      email : email 
+    };
+    
+    fetch('http://127.0.0.1:5000/a/v1/user/reset', {
+      method: 'POST',
+      headers: {
+              'Content-Type': 'application/json' ,
+              'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify(dataToSend)
+    })
+    .then(response => {
+      if (!response.ok){
+        throw new Error('Network response was not ok.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Server Response:', data);
+      alert('User password changed successfully');
+      location.reload();
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+      alert('Failed to send data.')
+    });
+
+});
